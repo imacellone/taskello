2b9803896034edcbc5981f9771b8e6b9d2b2ddf5
Italo Macellone <italomacellone@gmail.com>
2025-04-06 18:08:30 +0200

    initial project setup with spring boot and gradle

---
+++ src/main/java/com/taskello/TaskelloApiApplication.java
@@ -0,0 +1,13 @@
+package com.taskello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskelloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskelloApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskello/TaskelloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskelloApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
