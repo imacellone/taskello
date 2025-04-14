530d282a20735b46092eca6cf7f46837f743270b
Italo Macellone <italomacellone@gmail.com>
2025-04-14 21:18:56 +0200

    Don't include empty fields in the TaskOutput

--- src/main/java/com/taskello/api/v1/model/output/TaskOutput.java
+++ src/main/java/com/taskello/api/v1/model/output/TaskOutput.java
@@ -1,14 +1,18 @@
 package com.taskello.api.v1.model.output;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.OffsetDateTime;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
+
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@JsonInclude(NON_EMPTY)
 public class TaskOutput {
     private Long id;
     private String title;
